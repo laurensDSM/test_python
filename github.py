d464482e09ce40210d9defd145fae3148b2a8e3d
Laurens <laurens.de.smedt.pc@hotmail.com>
2023-06-05 22:00:15 +0200

    eerste stappen'

---
+++ github.py
@@ -0,0 +1,11 @@
+
+class Github():
+    def __init__(self,repo_url):
+        self.repo_url = repo_url
+
+    def get_config():
+        pass
+    def get_module():
+        pass
+    def log_data():
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+from github import Github
+
+github = Github()
+github.get_config()
+if __name__ == "__main__":
+    pass

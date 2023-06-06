44a4ed0868940477d6a0ce9b5f6672a4cfd2e7ad
laurens <s129534@ap.be>
2023-06-06 11:57:52 +0200

    test file

--- main.py
+++ main.py
@@ -4,15 +4,46 @@ import importlib
 import json
 import os
 from importlib import import_module
+import socket
+import datetime
+import random
+def create_directory_in_logs(directory_name):
+    """Deze functie zal een map aanmaken in de map logs op basis van de hostname + datum """
+    logs_directory = "logs"
+    new_directory_path = os.path.join(logs_directory, directory_name)
 
+    # Controleer of de logs-map bestaat
+    if not os.path.exists(logs_directory):
+        print(f"De map '{logs_directory}' bestaat niet.")
+        return
+
+    # Controleer of de nieuwe map al bestaat
+    if os.path.exists(new_directory_path):
+        return
+
+    # Maak de nieuwe map aan binnen de logs-map
+    os.makedirs(new_directory_path)
+    print(f"De map '{new_directory_path}' is succesvol aangemaakt.")
+
+
+def generate_unique_id():
+    """Deze functie zal unique id maken op basis van de hostname + datum + een random nummer"""
+    hostname = socket.gethostname()
+    current_day = datetime.date.today().strftime('%Y%m%d')
+    number = int(random.uniform(1, 2000))
+    unique_id = f"{hostname}_{current_day}_{number}"
+
+    return unique_id
 
 def main():
 #  github = Github('https://github.com/laurensDSM/test_python.git')
 #   print(github.check_update())
+    id = generate_unique_id()
+    create_directory_in_logs(id)
     config_path = "config/config.txt"  # Het pad naar het configuratiebestand
-
-    try:
-        with open(config_path, "r") as config_file:
+    print(id)
+    print(config_path)
+    with open(config_path, "r") as config_file:
             config = json.load(config_file)
             
             module_name = config[0]["module_name"]
@@ -28,15 +59,13 @@ def main():
             my_class = getattr(module, class_name)()
 
             # Gebruik de klasse
-            my_class.log_text()
+            my_class.log_text(id)
 
-                    
-    except FileNotFoundError:
-        print("Het configuratiebestand kon niet worden gevonden.")
-    except json.JSONDecodeError:
-        print("Het configuratiebestand bevat geen geldige JSON-indeling.")
-    except IOError:
-        print("Er is een fout opgetreden bij het openen van het configuratiebestand.")
+
+
+
+
+
 
 
 if __name__ == "__main__":
--- modules/13.py
+++ modules/13.py
@@ -1,13 +1,11 @@
 import os
 import datetime
 class Test_module():
-    def log_text(text = "Hallo ik ben een test"):
-        log_folder = "logs"
-        if not os.path.exists(log_folder):
-            os.makedirs(log_folder)
-        
+
+
+    def log_text(slef,id):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        log_file = f"{log_folder}/log_{timestamp}.txt"
-        
+        print(timestamp)
+        log_file = f"logs/{id}/log_{timestamp}.txt"
         with open(log_file, "a") as file:
-            file.write(f"{text}\n")
+            file.write(f"Hallo ik ben op {timestamp} besmet\n")
